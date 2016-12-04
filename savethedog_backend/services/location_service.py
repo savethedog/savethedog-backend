ddb84715e3e4987841dedc54d0cea5cf98194028
les <nanotrippolo@gmail.com>
2016-12-04 22:14:06 +0000

    added nearby announces lookup

--- savethedog_backend/services/location_service.py
+++ savethedog_backend/services/location_service.py
@@ -1,21 +1,37 @@
 from flask import jsonify
 from flask_restful import Resource, abort
+import pymongo
+from pymongo import MongoClient
+
+from utils.database.retrievers import MongoObjectsRetriever
 
 
-class LocationResource(Resource):
-
-    def __init__(self, authenticator):
+class NearLocationResource(Resource):
+    def __init__(self, authenticator, mongo):
         self.authenticator = authenticator
+        self.mongo = mongo
+        self.retriever = MongoObjectsRetriever()
 
-    def get(self, token):
+    def get(self, token, lat, long):
+        # if not self.authenticator.authenticate(auth_data=dict(token=token)):
+        #     abort(400)
 
-        if not self.authenticator.authenticate(auth_data=dict(token=token)):
-            abort(400)
+        nearby_items = self.mongo.get_narby_announces(lat, long, radius=1)
+        items = self.retriever.get_announces(nearby_items)
+        announces = {'announces': []}
 
-        return jsonify(dict(result="ok"))
+        for item in items:
+            announces['announces'].append(row2dict(item))
+
+        return jsonify(announces)
+
+    def post(self):
+        pass
 
 
+def row2dict(row):
+    d = {}
+    for column in row.__table__.columns:
+        d[column.name] = str(getattr(row, column.name))
 
-
-
-
+    return d
